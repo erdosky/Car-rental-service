43c74b4457265d871f714aab6003c1ac7f9adf2f
erdosky <rozwadowski.kontakt@gmail.com>
2023-11-27 19:41:47 +0100

    added enums and entities, also added packages for project skeleton

---
+++ car.rental.service/src/main/java/car/rental/service/com/example/car/rental/service/enums/CarStatus.java
@@ -0,0 +1,17 @@
+package car.rental.service.com.example.car.rental.service.enums;
+
+public enum CarStatus {
+    AVAILABLE("Available"),
+    UNAVAILABLE("Unavailable"),
+    RENTED("Rented");
+
+    private final String stringValue;
+
+    CarStatus(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+}
