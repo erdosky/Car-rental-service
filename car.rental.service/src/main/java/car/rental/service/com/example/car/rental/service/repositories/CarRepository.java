d8fd384f7fdb1e6ee91d7d86d303e10a3e54e2a9
erdosky <rozwadowski.kontakt@gmail.com>
2023-11-30 21:05:34 +0100

    tests for user,rental and car

--- car.rental.service/src/main/java/car/rental/service/com/example/car/rental/service/repositories/CarRepository.java
+++ car.rental.service/src/main/java/car/rental/service/com/example/car/rental/service/repositories/CarRepository.java
@@ -1,7 +1,6 @@
 package car.rental.service.com.example.car.rental.service.repositories;
 
 import car.rental.service.com.example.car.rental.service.entities.Car;
-import car.rental.service.com.example.car.rental.service.enums.CarType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,7 +8,7 @@ import java.util.List;
 public interface CarRepository extends JpaRepository <Car, Long> {
 
     Car findByModel(String model);
-    List <Car> findAllByType(CarType type);
+    List <Car> findAllByType(String type);
 
     List <Car> findAllByBrand(String brand);
 
